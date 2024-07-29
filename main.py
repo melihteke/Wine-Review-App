11edb76a9bb75b3d9de0125567087ae2ba348c31
mteke1_nike <melihteke@gmail.com>
2024-07-29 10:08:50 +0200

    added fask_restx

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
 from flask import Flask
 import os
+from flask import Flask, render_template
+from flask_restx import Api, Resource, reqparse
 
-
+# Initialize Flask app and Api
 app = Flask(__name__)
+api = Api(app, title='Heroku API', version='1.0', description='API collection for www.mteke.com applications')
 
-@app.route('/')
-def hello_world():
-    NAME = os.environ['NAME']
-    SURNAME = os.environ['SURNAME']
-    return f'Hello, {NAME} {SURNAME}!'
+# Define a Retail Voice namespace
+ns1 = api.namespace('', description='mteke.com APIs')
+
+@ns1.route('/')
+class HelloWorld(Resource):
+    def get(self):
+        NAME = os.environ['NAME']
+        SURNAME = os.environ['SURNAME']
+        return f'Hello, {NAME} {SURNAME}!'
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(debug=True, port=5001)
